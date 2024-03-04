31816354a11b10ff3fd69fdd5319d3f16b1f15bd
Basavaraj Magadum <basavarajm@infinite.com>
2024-03-04 15:09:58 +0530

    Assessments

--- Assesments/Assessment_1/Assessment_1/Properties/AssemblyInfo.cs
+++ Assesments/Assessment_1/Assessment_1/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Assesments")]
+[assembly: AssemblyTitle("Assessment_1")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Assesments")]
+[assembly: AssemblyProduct("Assessment_1")]
 [assembly: AssemblyCopyright("Copyright ©  2024")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("3461d653-3e58-4522-971f-577ed754bb3c")]
+[assembly: Guid("cbd905d3-0907-4bba-8f70-afdda32d3ef2")]
 
 // Version information for an assembly consists of the following four values:
 //
