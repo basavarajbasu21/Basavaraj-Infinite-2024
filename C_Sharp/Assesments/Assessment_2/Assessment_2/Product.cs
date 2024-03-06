0da549db064f64bbe70decd7c975f21aa4e27704
Basavaraj Magadum <basavarajm@infinite.com>
2024-03-06 10:32:43 +0530

    Assessment 2

---
+++ C_Sharp/Assesments/Assessment_2/Assessment_2/Product.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Product
+{
+    class Product
+    {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public double Price { get; set; }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("----- WEL COME TO SHOP -----");
+
+            
+            Product[] products = new Product[10];
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine($"Enter details for Product {i + 1}:");
+
+                products[i] = new Product
+                {
+                    ProductId = i + 1,
+                    ProductName = Console.ReadLine(),
+                    Price = GetValidDoubleInput("Enter Price: ")
+                };
+            }
+
+            // Sort products based on price
+            Array.Sort(products, (p1, p2) => p1.Price.CompareTo(p2.Price));
+
+            // Display sorted products
+            Console.WriteLine("\nSorted Products based on Price:");
+            double totalPrice = 0;
+
+            foreach (var product in products)
+            {
+                Console.WriteLine($"Product ID: {product.ProductId}, Product Name: {product.ProductName}, Price: {product.Price:C}");
+            }
+
+            Console.WriteLine("----- Total Price of all products -----");
+            Console.WriteLine($"\nTotal Price : {totalPrice:C}");
+
+            Console.WriteLine("----- THANK YOU -----");
+            Console.WriteLine("      VISIT AGAIN      ");
+
+
+            Console.ReadLine();
+        }
+
+        static double GetValidDoubleInput(string prompt)
+        {
+            double result;
+            do
+            {
+                Console.Write(prompt);
+            } while (!double.TryParse(Console.ReadLine(), out result));
+
+            return result;
+        }
+    }
+}
---
+++ C_Sharp/Assesments/Assessment_2/Assessment_2/Student.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Student
+{
+    abstract class Student
+    {
+        public string Name { get; set; }
+        public int StudentId { get; set; }
+        public double Grade { get; set; }
+
+        public abstract bool IsPassed(double grade);
+    }
+
+    class Undergraduate : Student
+    {
+        public override bool IsPassed(double grade)
+        {
+            return grade > 70.0;
+        }
+    }
+
+    class Graduate : Student
+    {
+        public override bool IsPassed(double grade)
+        {
+            return grade > 80.0;
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("----- Abstract class called  Student with  Name, StudentId, Grade as members -----");
+
+            // Test with Undergraduate student
+            Undergraduate undergradStudent = new Undergraduate
+            {
+                Name = "BASAVARAJ MAGADUM",
+                StudentId = 1034337,
+                Grade = 75.0
+            };
+
+
+
+            Console.WriteLine($"Undergraduate Student: {undergradStudent.Name}, ID: {undergradStudent.StudentId}, Grade: {undergradStudent.Grade}");
+            Console.WriteLine($"Is Passed: {undergradStudent.IsPassed(undergradStudent.Grade)}\n");
+            //Console.ReadLine();
+
+            // Test with Graduate student
+            Graduate gradStudent = new Graduate
+            {
+                Name = "SHANTESH DESAI",
+                StudentId = 9353468,
+                Grade = 85.0
+            };
+
+            Console.WriteLine($"Graduate Student: {gradStudent.Name}, ID: {gradStudent.StudentId}, Grade: {gradStudent.Grade}");
+            Console.WriteLine($"Is Passed: {gradStudent.IsPassed(gradStudent.Grade)}");
+            Console.ReadLine();
+        }
+    }
+}
