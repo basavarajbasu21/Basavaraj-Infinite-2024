faae9c00e9d0a624e4d45cdf72c76634a4c72205
basavarajbasu21 <161022060+basavarajbasu21@users.noreply.github.com>
2024-04-21 00:28:40 +0530

    Assignment Registration Form 1

---
+++ Assignments/ASPNet/Assignment_ASP_1/Assignment_ASP_1/WebFormVali.aspx.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Assignment_ASP_1
+{
+    public partial class WebFormVali : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        protected void btnregister_Click(object sender, EventArgs e)
+        {
+            if (Page.IsValid)
+            {
+                Response.Redirect("Welcome.html");
+            }
+        }
+    }
+}
